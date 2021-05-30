5b3da86551a0e5fd2b6c3cc4eedb377e39853f76
Gourav <gore.auluck@gmail.com>
2021-05-29 22:15:25 -0700

    Navbar style and responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
 import { menuData } from "../data/MenuData";
+import { Button } from "./Button";
+import Bars from "../images/bars.svg";
 
 const Navbar = () => {
   return (
@@ -15,6 +17,11 @@ const Navbar = () => {
           </NavMenuLinks>
         ))}
       </NavMenu>
+      <NavBtn>
+        <Button to="/contact" primary="true">
+          Contact Us
+        </Button>
+      </NavBtn>
     </Nav>
   );
 };
@@ -26,18 +33,65 @@ const Nav = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1rem;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
   background: tomato;
 `;
 
-const Logo = styled(Link)`
+const NavLink = css`
   color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
 `;
 
-const MenuBars = styled.i``;
+const Logo = styled(Link)`
+  ${NavLink}
+  font-style: italic;
+`;
 
-const NavMenu = styled.div``;
+const MenuBars = styled.i`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    background-image: url(${Bars});
+    background-size: contain;
+    height: 40px;
+    width: 40px;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-50%, 25%);
+  }
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
 const NavMenuLinks = styled(Link)`
-  color: #fff;
+  ${NavLink}
+`;
+
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
